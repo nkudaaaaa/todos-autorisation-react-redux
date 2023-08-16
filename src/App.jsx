b4855a9b347ba3bf916fd5f5de77e4f5dd657ebd
Nikita <130391492+nkudaaaaa@users.noreply.github.com>
2023-08-16 20:47:07 +0300

    fully ready autorisation window

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-
-// import { Provider } from 'react-redux';
-// import store from './features/store';
-// import TaskList from './features/TaskList';
-// import AddTaskForm from './features/AddTaskForm';
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <div>
-//         <h1>Управление задачами с Redux</h1>
-//         <AddTaskForm />
-//         <TaskList />
-//       </div>
-//     </Provider>
-//   );
-// }
-
-// export default App;
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { useSelector } from 'react-redux';
 import { addUser } from './features/UsersStore';
@@ -26,10 +7,10 @@ import { useDispatch } from 'react-redux';
 
 
 function App() {
-
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [isSigned, setIsSigned] = useState(true);
+
     const users = useSelector(state => state);
     const dispatch = useDispatch();
     console.log(users);
@@ -58,17 +39,30 @@ function App() {
 
     const handleClickSignUp = () => {
       dispatch(addUser({id: Date.now(), login: login, password: password}));
-      alert('sdf')
+      alert('You registred successfully')
       setLogin('');
       setPassword('');
       setIsSigned(true);
     }
+
+    const handleTip = () => {
+      console.log('in');
+      setIsSigned(prev => {
+        console.log("Previous value:", prev);
+        const newValue = !prev;
+        console.log("New value:", newValue);
+        return newValue;
+      });
+      setLogin('');
+      setPassword('');
+    }
+
   return (
     <>
         <div className="auto-window">
           <div className="container">
 
-                <span className='text'>Hello, Welcome again!</span> <br />
+                <span className='text'>{isSigned ? 'hello, welome again!': 'registration'}</span> <br />
                 <div className="inputs">
                   <input 
                     type="text"
@@ -87,12 +81,12 @@ function App() {
                 </div>
                 <div className="submittion">
                   {
-                    isSigned ? <button type='submit' onClick={handleClickLogIn}>LOG IN</button> :
-                    <button type='submit' onClick={handleClickSignUp}>SIGN UP</button>
+                    isSigned ? <button type='submit' className='button' onClick={handleClickLogIn}>LOG IN</button> :
+                    <button type='submit' className='button' onClick={handleClickSignUp}>SIGN UP</button>
                   }
                   {isSigned ? 
-                  <a href="#" onClick={() => setIsSigned(prev => !prev)}><span><u>Dont have an account? Sign up</u></span></a> : 
-                  <a href="#" onClick={() => setIsSigned(prev => !prev)}><span><u>Already have an account? Log in</u></span></a>
+                  <button className='button-no-backgr' onClick={handleTip}><span><u>Dont have an account? Sign up</u></span></button> : 
+                  <button className='button-no-backgr' onClick={handleTip}><span><u>Already have an account? Log in</u></span></button>
                   } <br /> <br />            
                 </div>
           </div>
